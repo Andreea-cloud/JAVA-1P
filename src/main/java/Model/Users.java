6a819abfdd5754b4d6394ac7eefecc45e3eb0eaa
Andreea Nicut <andreea.nicut30@gmail.com>
2020-06-04 21:17:05 +0300

    Add some Users class

---
+++ src/main/java/Model/Users.java
@@ -0,0 +1,17 @@
+package Model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Users {
+    int id;
+    String username;
+    String password;
+    String email;
+}
+
