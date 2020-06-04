21a51cd391f9e159654fb6776237f34f3c0e3a74
Andreea-cloud <66202708+Andreea-cloud@users.noreply.github.com>
2020-06-04 20:38:11 +0300

    First commit

---
+++ src/main/java/View/TestPage.java
@@ -0,0 +1,24 @@
+package View;
+
+public class TestPage {
+
+    public static void main(String[] args) {
+        StartPage start = new StartPage();
+
+        //LoginPage login = new LoginPage();
+        //MyAccountPage cont = new MyAccountPage();
+        //AddFlightPage add = new AddFlightPage();
+       // MainController mainController = new MainController();
+
+
+       // mainController.adaugaUser("ANDREEA", "123S", "AA@g.h");
+       // List<Users> tmp =  mainController.getUsers();
+
+      //  for(Users user : tmp) {
+           // System.out.println(user.getUsername());
+       // }
+
+            System.out.println("Corect");
+    }
+}
+
